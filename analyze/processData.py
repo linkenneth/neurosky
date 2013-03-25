e152cfdedeb0f694ff13818bdc3662dc69a0f3a7
michaellin <michaelv03@gmail.com>
2013-03-24 18:43:30 -0700

    "changed directories"

--- analyze/processData.py
+++ analyze/processData.py
@@ -29,32 +29,3 @@ while True:
             continue
         else:
             print line
-
-def processBlink (accuracy, THR):
-""""processBlink 
-        input: int accuracy how many sample to use for detection
-               THR - threshold for triggering a detection
-        output: process, pass the samples to this function"""
-    SSIZE = accuracy
-    i = 0
-    accum = 0
-    def process (sample):
-        """processoutput: 1 - detected blink
-                          0 - need more samples doesnt know yet
-                         -1 - detected no blinks"""
-        nonlocal SSIZE, accum, i
-        if (i != SSIZE):
-           accum += sample
-           return 0
-        else:
-           i = 0
-           if (accum/sample > THR):
-               return 1
-           else:
-               return -1
-    return process
-
-
-        
-    
-
