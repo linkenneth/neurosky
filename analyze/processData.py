4ff4dca94aeb2dd76f1f3bd56465dd6a170678ff
michaellin <michaelv03@gmail.com>
2013-03-28 19:27:52 -0700

    "cleaning working dir"

--- analyze/processData.py
+++ analyze/processData.py
@@ -1,35 +1,52 @@
-import serial
+import time
 import subprocess
+import serial
+import processBlink
 
-SERIAL_NUM = 0
+def initserial():
+    s = serial.Serial()
+    s.port = "/dev/tty.usbserial-AH01KXWW"
+    s.baudrate = 115200
+    s.open()
+    return s
 
-def main(process_turn, process_forward):
-    proc = subprocess.Popen("./analyze.out", shell=True, stdout=subprocess.PIPE)
-    poor = True
-    ser = serial.Serial(SERIAL_NUM)
-    turn_state = 'R'
-    forward_state = 'F'
+def broadcast(data):
+    """ Helper functions to broadcast to serial port""" 
+    s.write(bytes(data))
 
-    while True:
-        line = proc.stdout.readline()
-        if line != '':
-            signal = line.split(": ")
-            print signal[0], signal[1]
-            if signal[0] == "POOR_SIGNAL":
-                poor = int(signal[1]) != 0
-            elif poor:
-                continue
+proc = subprocess.Popen("analyze.out", shell = True, stdout = subprocess.PIPE)
+
+s = initserial()
+poor = False
+SAMPLE_LEN = 10
+direction = 'L'
+sProcessor = processBlink(SAMPLE_LEN, 200) #signal processor
+duration = 10 #hardcoded duration
+
+while True:
+    line = proc.stdout.readline()
+    if line != '':
+        signal = line.split(": ")
+        if signal[0] == "POOR_SIGNAL":
+            if int(signal[1]) == 200:
+                poor = True
             else:
-                signal = signal[1]
-                turn = process_turn(signal)
-                forward = process_forward(signal)
-                if turn:
-                    turn_state = 'R' if 'L' else 'R'
-                    ser.write(turn_state)
-                if forward:
-                    forward_state = 'F' if 'B' else 'F'
-                    ser.write(forward_state)
-
-if __name__ == "__main__":
-    pass
-    # main(FUNCTIONS HERE)
+                poor = False
+            print(line)
+        elif poor:
+            continue
+        if signal[0] == "RAW":
+            rsp = sProcessor(signal[1])
+            if (rsp == 1):
+                direction = 'R' if direction == 'L' else 'L' #toggle direction
+                broadcast(direction) #Send command through serial
+        elif signal[0] == "MEDITATION":
+            if int(signal[1]) > 55:
+                # move forward
+                broadcast("F")
+            else:
+                # stop
+                broadcast("S")
+            print(line)
+        else:
+            print(line)
