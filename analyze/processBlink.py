ec5477248ad1c5747ca385778a7b193a4f1c4125
Kenneth Lin <kenlin@berkeley.edu>
2013-04-10 00:29:06 -0700

    works?

--- analyze/processBlink.py
+++ analyze/processBlink.py
@@ -5,8 +5,8 @@ def processBlink (accuracy, THR):
     THR - threshold for triggering a detection.
     output: process, pass the samples to this function """
     SSIZE = accuracy
-    MIN = -9999
-    MAX = 9999
+    MIN = 9999
+    MAX = -9999
     i = 1
     accum = 0
     def process (sample):
@@ -14,24 +14,25 @@ def processBlink (accuracy, THR):
                           0 - need more samples doesnt know yet
                          -1 - detected no blinks"""
         nonlocal SSIZE, MIN, MAX, accum, i
-        if (i == SSIZE*4):
+        accum += sample
+        if (i % SSIZE) == 0: # For grouping
+           avg = accum / SSIZE  # python 3 floatdiv
+           if avg < MIN:
+               MIN = avg
+           if avg > MAX:
+               MAX = avg
+           accum = 0
+        if i == SSIZE * 4:
            i = 1
            diff = MAX - MIN
-           MAX = 9999
-           MIN = -9999
-           if (diff > THR):
+           MAX = -9999
+           MIN = 9999
+           if diff > THR:
                return 1
            else:
                return -1
-        accum += sample
-        i += 1
-        if ((i % SSIZE) == 0): #For grouping
-           avg = accum/SSIZE
-           if (avg < MIN or MIN == -9999):
-               MIN = avg
-           if (avg > MAX or MAX == 9999):
-               MAX = avg
-           accum = 0
-           i += 1
+        else:
+            i += 1
+            return 0
 
     return process
