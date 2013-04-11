84ac078b88516320dac956d03450e32793433f83
Kenneth Lin <kenlin@berkeley.edu>
2013-04-10 19:11:03 -0700

    using datetime to benchmark instead

--- analyze/fourier.py
+++ analyze/fourier.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import subprocess
+from datetime import datetime
 
 BUFFER_SIZE = 256
 SAMPLING_RATE = 512.0 # Hz
@@ -22,12 +23,12 @@ plt.show()
 
 def detect(freq, fft, thr):
     accum = 0
-    for i in len(fft):
+    for i in range(len(fft)):
         if 0 < freq[i] < 10:
             accum += fft[i]
     return accum > thr
 
-totstart = time.clock()
+totstart = datetime.now()
 while True:
     line = unicode(proc.stdout.readline(), encoding="utf8")
     if line != '':
@@ -38,16 +39,16 @@ while True:
             buf[i] = int(signal[1])
             i += 1
             if i % BUFFER_SIZE == 0:
-                start = time.clock()
+                start = datetime.now()
                 sp = np.fft.fft(buf, n=250)
-                print "fft: %f" % (time.clock() - start)
+                print "fft: %f" % (datetime.now() - start).microseconds
                 freq = np.fft.fftfreq(sp.size) * SAMPLING_RATE
                 start = time.clock()
                 rline.set_data(freq, sp.real)
-                print "rline: %f" % (time.clock() - start)
+                print "rline: %f" % (datetime.now() - start).microseconds
                 start = time.clock()
                 plt.draw()
-                print "plot: %f" % (time.clock() - start)
+                print "plot: %f" % (datetime.now() - start).microseconds
                 i = 0
-                print "tot: %f" % (time.clock() - totstart)
-                totstart = time.clock()
+                print "tot: %f" % (datetime.now() - totstart).microseconds
+                totstart = datetime.now()
