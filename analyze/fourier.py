13281be8b1780d8fdf662af970f4902edc148415
michaellin <michaelv03@gmail.com>
2013-04-10 18:59:32 -0700

    "benchmarking"

--- analyze/fourier.py
+++ analyze/fourier.py
@@ -15,11 +15,12 @@ proc = subprocess.Popen("./analyze.out", shell = True, stdout = subprocess.PIPE)
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
 ax.set_xlim(-0.1*SAMPLING_RATE, 0.1*SAMPLING_RATE)
-ax.set_ylim(0, 10000)
+ax.set_ylim(-10000, 10000)
 rline, = ax.plot(0, 0)
 plt.ion()
 plt.show()
 
+totstart = time.clock()
 while True:
     line = unicode(proc.stdout.readline(), encoding="utf8")
     if line != '':
@@ -30,8 +31,16 @@ while True:
             buf[i] = int(signal[1])
             i += 1
             if i % BUFFER_SIZE == 0:
+                start = time.clock()
                 sp = np.fft.fft(buf, n=250)
+                print "fft: %f" % (time.clock() - start)
                 freq = np.fft.fftfreq(sp.size) * SAMPLING_RATE
+                start = time.clock()
                 rline.set_data(freq, sp.real)
+                print "rline: %f" % (time.clock() - start)
+                start = time.clock()
                 plt.draw()
+                print "plot: %f" % (time.clock() - start)
                 i = 0
+                print "tot: %f" % (time.clock() - totstart)
+                totstart = time.clock()
